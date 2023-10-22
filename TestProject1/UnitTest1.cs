7b4ef6fe31200d5820e5a633c30763ad72eedd7a
pl_konstkri <fcbsport2015@bk.ru>
2023-10-23 00:48:04 +0300

    написал тесты для Calculator

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,92 @@
+using Testing_lab_1.lab_1.classes;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CalculatorSumTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+            double a = 100, b = 200, result = 0;
+
+            // Act
+            result = calculator.Sum(a, b);
+
+            // Assert
+            Assert.Equal(300, result);
+        }
+
+        [Fact]
+        public void CalculatorSubtractTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+            double a = 100, b = 200, result = 0;
+
+            // Act
+            result = calculator.Subtract(a, b);
+
+            // Assert
+            Assert.Equal(-100, result);
+        }
+
+        [Fact]
+        public void CalculatorDIvideTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+            double a = 100, b = 200, result = 0;
+
+            // Act
+            result = calculator.Divide(a, b);
+
+            // Assert
+            Assert.Equal(0.5, result);
+        }
+
+        [Fact]
+        public void CalculatorMultiplyTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+            double a = 100, b = 200, result = 0;
+
+            // Act
+            result = calculator.Multiply(a, b);
+
+            // Assert
+            Assert.Equal(20000, result);
+        }
+
+        [Fact]
+        public void CalculatorMultiplyOnZeroTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+            double a = 100, b = 0, result = 0;
+
+            // Act
+            result = calculator.Multiply(a, b);
+
+            // Assert
+            Assert.Equal(double.NaN, result);
+        }
+
+
+        [Fact]
+        public void CalculatorMultiplyByMinimumTest()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+            double a = 100, b = 10e-9, result = 0;
+
+            // Act
+            result = calculator.Multiply(a, b);
+
+            // Assert
+            Assert.Equal(double.NaN, result);
+        }
+    }
+}
