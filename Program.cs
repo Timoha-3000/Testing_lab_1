15ad51e7518ed7c06b4004ab69b4afafdb997638
pl_konstkri <fcbsport2015@bk.ru>
2023-10-22 20:18:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Testing_lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Tests_for_1st_lab/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tests_for_1st_lab
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
